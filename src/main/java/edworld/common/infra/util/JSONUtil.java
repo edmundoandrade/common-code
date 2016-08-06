04f256aa172d02f80430a02f7b757b38b3fbd12d
edmundoandrade <edmundo@lia.ufc.br>
2016-08-05 23:22:25 -0300

    Refactoring

--- src/main/java/edworld/common/infra/util/JSONUtil.java
+++ src/main/java/edworld/common/infra/util/JSONUtil.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.Map;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig.Feature;
@@ -12,10 +13,10 @@ import org.codehaus.jackson.type.TypeReference;
 public abstract class JSONUtil {
 	private static ObjectMapper objectMapper;
 
-	public static String toString(Object objeto) {
+	public static String toString(Object object) {
 		StringWriter writer = new StringWriter();
 		try {
-			objectMapper().writeValue(writer, objeto);
+			objectMapper().writeValue(writer, object);
 			String result = writer.toString();
 			writer.close();
 			return result;
@@ -24,18 +25,23 @@ public abstract class JSONUtil {
 		}
 	}
 
-	public static void toFile(Object objeto, File arquivoDestino) {
-		arquivoDestino.getParentFile().mkdirs();
+	public static void toFile(Object object, File targetFile) {
+		targetFile.getParentFile().mkdirs();
 		try {
-			objectMapper().writeValue(arquivoDestino, objeto);
+			objectMapper().writeValue(targetFile, object);
 		} catch (IOException e) {
 			throw new IllegalArgumentException(e);
 		}
 	}
 
-	public static <T> T fromString(String conteudo, TypeReference<T> typeRef) {
+	public static Map<String, String> toMap(String jsonText) {
+		return JSONUtil.fromString(jsonText, new TypeReference<Map<String, String>>() {
+		});
+	}
+
+	public static <T> T fromString(String jsonText, TypeReference<T> typeRef) {
 		try {
-			return objectMapper().readValue(conteudo, typeRef);
+			return objectMapper().readValue(jsonText, typeRef);
 		} catch (IOException e) {
 			throw new IllegalArgumentException(e);
 		}
