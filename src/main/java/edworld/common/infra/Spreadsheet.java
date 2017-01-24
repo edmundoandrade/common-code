ddc3b0ec41c2feceb0f66933adea1aa5ba304b8a
edmundoandrade <edmundo@lia.ufc.br>
2017-01-24 18:18:21 -0200

    Include support to XSL files.

--- src/main/java/edworld/common/infra/Spreadsheet.java
+++ src/main/java/edworld/common/infra/Spreadsheet.java
@@ -3,17 +3,30 @@ package edworld.common.infra;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Spreadsheet {
-	private XSSFWorkbook workbook;
-	private XSSFSheet sheet;
+	public enum SpreadsheetFormat {
+		XLS, XLSX
+	}
+
+	private Workbook workbook;
+	private Sheet sheet;
 
 	public Spreadsheet(InputStream stream) throws IOException {
-		workbook = new XSSFWorkbook(stream);
+		this(stream, SpreadsheetFormat.XLSX);
+	}
+
+	public Spreadsheet(InputStream stream, SpreadsheetFormat format) throws IOException {
+		if (format.equals(SpreadsheetFormat.XLS))
+			workbook = new HSSFWorkbook(stream);
+		else
+			workbook = new XSSFWorkbook(stream);
 		sheet = workbook.getSheetAt(0);
 	}
 
@@ -22,10 +35,10 @@ public class Spreadsheet {
 	}
 
 	public String cell(int rowNumber, int columnNumber) {
-		XSSFRow row = sheet.getRow(rowNumber - 1);
+		Row row = sheet.getRow(rowNumber - 1);
 		if (row == null)
 			return null;
-		XSSFCell col = row.getCell(columnNumber - 1);
+		Cell col = row.getCell(columnNumber - 1);
 		if (col == null)
 			return null;
 		return col.toString().trim();
