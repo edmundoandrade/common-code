699d845c5a7072c3074b0adabaf51c4b839115b4
edmundoandrade <edmundo@lia.ufc.br>
2018-02-15 16:16:54 -0200

    HTMLUtil decodes URL parameters

--- src/main/java/edworld/common/infra/util/HTMLUtil.java
+++ src/main/java/edworld/common/infra/util/HTMLUtil.java
@@ -6,6 +6,7 @@ import static org.apache.commons.text.StringEscapeUtils.unescapeHtml4;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import edworld.common.core.Link;
+import edworld.common.infra.Config;
 
 public class HTMLUtil {
 	public static final String LF = System.getProperty("line.separator");
@@ -43,9 +45,25 @@ public class HTMLUtil {
 		return extractSegmentsHTML(tableRow, Pattern.compile("(?is)<t[dh][^>]*>.*?</t[dh]>"));
 	}
 
-	public static String encodeURLParam(String parametro) {
+	public static String encodeURLParam(String parameter) {
+		return encodeURLParam(parameter, Config.getEncoding());
+	}
+
+	public static String encodeURLParam(String parameter, String encoding) {
 		try {
-			return URLEncoder.encode(parametro, "UTF-8");
+			return URLEncoder.encode(parameter, encoding);
+		} catch (UnsupportedEncodingException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
+	public static String decodeURLParam(String parameter) {
+		return decodeURLParam(parameter, Config.getEncoding());
+	}
+
+	public static String decodeURLParam(String parameter, String encoding) {
+		try {
+			return URLDecoder.decode(parameter, encoding);
 		} catch (UnsupportedEncodingException e) {
 			throw new IllegalArgumentException(e);
 		}
--- src/test/java/edworld/common/infra/util/HTMLUtilTest.java
+++ src/test/java/edworld/common/infra/util/HTMLUtilTest.java
@@ -11,4 +11,16 @@ public class HTMLUtilTest {
 		assertEquals("Sessão Solene", HTMLUtil.textHTML("<b><span>Sessão Solene<o:p></o:p></span></b>"));
 		assertEquals("Debater \"TEMA\" já.", HTMLUtil.textHTML("<p>&nbsp;Debater <b>\"TEMA\"</b><em> já.</em></p>"));
 	}
+
+	@Test
+	public void encodeURLParam() {
+		assertEquals("Teste+com+acentua%C3%A7%C3%A3o+gr%C3%A1fica+e%2Fou+pontua%C3%A7%C3%A3o.",
+				HTMLUtil.encodeURLParam("Teste com acentuação gráfica e/ou pontuação."));
+	}
+
+	@Test
+	public void decodeURLParam() {
+		assertEquals("Teste com acentuação gráfica e/ou pontuação.",
+				HTMLUtil.decodeURLParam("Teste+com+acentua%C3%A7%C3%A3o+gr%C3%A1fica+e%2Fou+pontua%C3%A7%C3%A3o."));
+	}
 }
